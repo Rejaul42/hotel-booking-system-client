33fe514d228a720b6b4eef2c5b22fe2f3acbc334
Rejaul42 <rejaulkarimraju91@gmail.com>
2023-11-08 08:22:40 -0600

    sorting by room price

--- src/Pages/Rooms/Room.jsx
+++ src/Pages/Rooms/Room.jsx
@@ -3,13 +3,15 @@ import { CgProfile } from 'react-icons/cg';
 import { Link } from 'react-router-dom';
 
 const Room = ({ room }) => {
-    const { _id, room_type, image_url1, occupancy } = room
+    const { _id, room_type, image_url1, occupancy, price_per_night } = room
     return (
         <div>
             <div className="shadow-xl rounded-lg">
                 <figure><img className='h-72 w-96' src={image_url1} alt="Shoes" /></figure>
-                <div className="absolute text-white -mt-36 ">
+                <div className="absolute text-white -mt-48 ">
+                    <p className='bg-stone-500 w-24 p-2 text-white'>Price: ${price_per_night}</p>
                     <div className="p-4  justify-around items-center gap-4">
+
                         <h2 className=" text-2xl">{room_type}</h2>
                         <button className="btn btn-outline text-white"><Link to={`/rooms/details/${_id}`}>See Details</Link></button>
                         <p className='flex gap-2 items-center text-2xl'><CgProfile></CgProfile> {occupancy} Guests</p>
--- src/Pages/Rooms/Rooms.jsx
+++ src/Pages/Rooms/Rooms.jsx
@@ -1,19 +1,58 @@
 
+import { useState } from "react";
 import Room from "./Room";
 import { useLoaderData } from 'react-router-dom';
 
 
 const Rooms = () => {
-    const loadRooms = useLoaderData()
+    const loadData = useLoaderData()
+    const [loadedRooms, setLoadedRooms] = useState(loadData);
+    const compare = (a, b, ascendingOrder) => {
+        if (a < b) {
+            return ascendingOrder ? -1 : 1;
+        }
+        if (a > b) {
+            return ascendingOrder ? 1 : -1;
+        }
+        return 0;
+    }
+
+    const handleChange = (value) => {
+        if (value == 'none') {
+            setLoadedRooms([...loadData])
+        } else {
+            let toType, toAscending
+            switch (value) {
+                case 'ascending': toType = true; toAscending = true; break;
+                case 'descending': toType = true; toAscending = false; break;
+                case 'high': toType = false; toAscending = true; break;
+                case 'low': toType = false; toAscending = false; break;
+            }
+            let current = [...loadData]
+            current.sort((a, b) => toType ?
+                compare(a._id, b._id, toAscending)
+                :
+                compare(a.price_per_night, b.price_per_night, toAscending))
+            setLoadedRooms([...current])
+        }
+    }
+
     // console.log(loadedRooms)
     return (
-        <div className="my-16"> 
+        <div className="my-16">
             <h1 className="text-5xl text-center mb-8">Our Available Room</h1>
+            <form className="text-center">
+                <select  onChange={(e) => handleChange(e.target.value)} className="select select-bordered w-full max-w-xs">
+                    <option disabled selected value='none'>Sorting by Price</option>
+                    <option value="high">Low to Heigh Price</option>
+                    <option value="low">Heigh to Low Price</option>
+                </select>
+            </form>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-center my-12">
-            {
-                loadRooms?.map(room => <Room key={room._id} room={room}></Room>)
-            }
-        </div>
+                {
+                    loadedRooms?.map(room => <Room key={room._id} room={room}></Room>)
+                }
+            </div>
         </div>
     );
 };
