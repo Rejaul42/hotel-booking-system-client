6206636ab0b92d4f586d80c8b05d9ccf4c6529dc
Rejaul42 <rejaulkarimraju91@gmail.com>
2023-11-08 07:27:19 -0600

    update booking roome date

--- src/Pages/UpdateBook/UpdateBook.jsx
+++ src/Pages/UpdateBook/UpdateBook.jsx
@@ -1,12 +1,61 @@
 import { useLoaderData } from "react-router-dom";
+import Swal from "sweetalert2";
 
 
 const UpdateBook = () => {
     const loadedData = useLoaderData()
     console.log(loadedData)
+    const {_id, checkIn, checkOut } = loadedData;
+
+    const handleUpdate = (e) => {
+        e.preventDefault()
+        const form = e.target;
+        const checkIn = form.checkIn.value;
+        const checkOut = form.checkOut.value;
+        console.log(checkIn, checkOut)
+        const data ={
+            checkIn,
+            checkOut
+        }
+
+        fetch(`http://localhost:5000/booked/${_id}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.modifiedCount > 0) {
+                    Swal.fire(
+                        'Successfully Updated an item!',
+                        'You clicked the button!',
+                        'success'
+                    )
+                }
+                form.reset()
+            })
+
+    }
     return (
         <div>
-            <p>Update</p>
+            <div className="bg-stone-500 p-4 h-[500px] flex justify-center items-center">
+                <form onSubmit={handleUpdate}>
+                    <div className="flex justify-around gap-4">
+                        <div className="space-y-4 bg-stone-900 p-4 rounded-lg">
+                            <p className="text-xl text-white">Check-In </p>
+                            <input className="px-4 py-3 rounded-md" type="date" defaultValue={checkIn} name="checkIn" id="" />
+                        </div>
+                        <div className="space-y-4 bg-stone-900 p-4 rounded-lg">
+                            <p className="text-xl text-white">Check-Out </p>
+                            <input className="px-4 py-3 rounded-md" type="date" defaultValue={checkOut} name="checkOut" id="" />
+                        </div>
+                    </div>
+                    <button className="btn w-full mt-8">Book Now</button>
+                </form>
+            </div>
         </div>
     );
 };
